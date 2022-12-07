ff1fd150bf460a39bbf1a56d2caee66fe4511dee
Matias Hernandez <matiasfh@gmail.com>
2022-12-07 09:54:26 -0300

    add applescript to be run on raycast

---
+++ invoke-change-source.applescript
@@ -0,0 +1,2 @@
+set the currentAudioSource to (do shell script "~/Development/switch-audio-source/target/debug/switch-audio-source")
+display notification currentAudioSource
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ fn main() {
     let mut current_command = Command::new(SWITCH_AUDIO_SOURCE_PATH);
     current_command.arg("-c"); 
     current_command.stdout(Stdio::piped());
-current_command.stderr(Stdio::piped());
+    current_command.stderr(Stdio::piped());
     let current = current_command.execute_output().unwrap();
     let current_str =String::from_utf8(current.stdout).unwrap();
-    println!("{}", current_str);
     
     let mut command = Command::new(SWITCH_AUDIO_SOURCE_PATH);
 
@@ -23,18 +22,18 @@ current_command.stderr(Stdio::piped());
     command.stdout(Stdio::piped());
     command.stderr(Stdio::piped());
 
-let output = command.execute_output().unwrap();
+    let output = command.execute_output().unwrap();
     if let Some(exit_code) = output.status.code() {
-    if exit_code == 0 {
-        println!("Ok.");
+        if exit_code == 0 {
+            println!("Ok.");
+        } else {
+            eprintln!("Failed.");
+        }
     } else {
-        eprintln!("Failed.");
+        eprintln!("Interrupted!");
     }
-} else {
-    eprintln!("Interrupted!");
-}
     println!("{}", String::from_utf8(output.stdout).unwrap());
-println!("{}", String::from_utf8(output.stderr).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
 
 
 
